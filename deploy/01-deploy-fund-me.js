839d57131c9910d3e622c1ea05ff06bf6438bb66
Andy Hoffman <antibland@gmail.com>
2022-10-05 13:55:18 -0700

    feat: allow auto-switching between local chain, testnet, or mainnet

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,6 +1,6 @@
 const { network } = require("hardhat")
-const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
@@ -8,29 +8,32 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const chainId = network.config.chainId
 
     let ethUsdPriceFeedAddress
-    if (chainId == 31337) {
+    if (developmentChains.includes(network.name)) {
         const ethUsdAggregator = await deployments.get("MockV3Aggregator")
         ethUsdPriceFeedAddress = ethUsdAggregator.address
     } else {
         ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
     }
-    log("----------------------------------------------------")
-    log("Deploying FundMe and waiting for confirmations...")
+
+    // if the contract doesn't exist, deploy a minimal version of it
+    // for local teseting
+
+    const args = [ethUsdPriceFeedAddress]
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: [ethUsdPriceFeedAddress],
+        args, // put price feed address
         log: true,
-        // we need to wait if on a live network so we can verify properly
-        waitConfirmations: network.config.blockConfirmations || 1,
+        waitConfirmations: network.config.blockConfirmations,
     })
-    log(`FundMe deployed at ${fundMe.address}`)
 
     if (
         !developmentChains.includes(network.name) &&
         process.env.ETHERSCAN_API_KEY
     ) {
-        await verify(fundMe.address, [ethUsdPriceFeedAddress])
+        await verify(fundMe.address, args)
     }
+
+    log("------------------------------------------------")
 }
 
 module.exports.tags = ["all", "fundme"]
